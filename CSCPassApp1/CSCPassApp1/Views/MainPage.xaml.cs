f1f614c68a05d5dd68751b3c3adbf8ca3b3c5176
Nipun Birla <nipun.birla@gmail.com>
2015-05-11 01:21:46 +0530

    Project so far

---
+++ CSCPassApp1/CSCPassApp1/Views/MainPage.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using CSCPassApp1.Resources;
+
+namespace CSCPassApp1
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+
+            // Sample code to localize the ApplicationBar
+            //BuildLocalizedApplicationBar();
+        }
+
+        private void password_GotFocus(object sender, RoutedEventArgs e)
+        {
+            password.Password = "";
+        }
+
+        private void password_LostFocus(object sender, RoutedEventArgs e)
+        {
+            password.Password = "gjsdgj";
+        }
+
+        private void LogInButton_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/Views/MainPivot.xaml", UriKind.Relative));
+        }
+
+        private void SignUpButton_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/Views/register.xaml", UriKind.Relative));
+        }
+        
+        
+
+
+        // Sample code for building a localized ApplicationBar
+        //private void BuildLocalizedApplicationBar()
+        //{
+        //    // Set the page's ApplicationBar to a new instance of ApplicationBar.
+        //    ApplicationBar = new ApplicationBar();
+
+        //    // Create a new button and set the text value to the localized string from AppResources.
+        //    ApplicationBarIconButton appBarButton = new ApplicationBarIconButton(new Uri("/Assets/AppBar/appbar.add.rest.png", UriKind.Relative));
+        //    appBarButton.Text = AppResources.AppBarButtonText;
+        //    ApplicationBar.Buttons.Add(appBarButton);
+
+        //    // Create a new menu item with the localized string from AppResources.
+        //    ApplicationBarMenuItem appBarMenuItem = new ApplicationBarMenuItem(AppResources.AppBarMenuItemText);
+        //    ApplicationBar.MenuItems.Add(appBarMenuItem);
+        //}
+    }
+}
---
+++ CSCPassApp1/CSCPassApp1/Views/register.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using System.Windows.Media;
+
+namespace CSCPassApp1.Views
+{
+    public partial class register : PhoneApplicationPage
+    {
+        public register()
+        {
+            InitializeComponent();
+        }
+
+        
+
+        private void cpass_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            if (pass.Password == cpass.Password)
+                match.Visibility = Visibility.Collapsed;
+            else
+                match.Visibility = Visibility.Visible;
+        }
+    }
+}
